b22f0838ca05f8284c46dccb0f90c8c4d16524ee
mlitman <awesomefat@gmail.com>
2022-03-29 12:52:29 -0500

    added array of Threads for no reason lol

--- Server/chatServer.java
+++ Server/chatServer.java
@@ -1,5 +1,6 @@
 import java.io.PrintStream;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class chatServer
@@ -7,11 +8,13 @@ public class chatServer
     public static void main(String[] args) throws Exception
     {
         ServerSocket s = new ServerSocket(2222);
+        ArrayList<ChatWorkerThread> theThreads = new ArrayList<ChatWorkerThread>();
         while(true)
         {
             System.out.println("Listenning for Connection...");
             Socket client = s.accept(); //blocks
             ChatWorkerThread t = new ChatWorkerThread(client);
+            theThreads.add(t);
             t.start();
         }
         
