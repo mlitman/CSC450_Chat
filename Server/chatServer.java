6016b1c9691dc1646ff6793b354cfb244a7eb9bc
mlitman <awesomefat@gmail.com>
2022-03-29 12:39:21 -0500

    updated for server threads

--- Server/chatServer.java
+++ Server/chatServer.java
@@ -7,13 +7,13 @@ public class chatServer
     public static void main(String[] args) throws Exception
     {
         ServerSocket s = new ServerSocket(2222);
-        System.out.println("Listenning for Connection...");
-        Socket client = s.accept(); //blocks
-        System.out.println("Connection Established...");
-        PrintStream clientOutput = new PrintStream(client.getOutputStream());
-        Scanner clientInput = new Scanner(client.getInputStream());
-        clientOutput.println("What is your name?");
-        String name = clientInput.nextLine();
-        System.out.println("read: " + name);
+        while(true)
+        {
+            System.out.println("Listenning for Connection...");
+            Socket client = s.accept(); //blocks
+            ChatWorkerThread t = new ChatWorkerThread(client);
+            t.start();
+        }
+        
     }
 }
